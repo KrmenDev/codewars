b03fd3aed755337446d8e5fcb03f75a1c4cd21c2
CarmenSM <contacto.cmsm@gmail.com>
2023-07-06 19:18:34 -0600

    challenge of the day

---
+++ 8kyu/under-pressure.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+//My Solution
+function doubleInteger(i) {
+    return i*2;
+  }
+
+  //CodeWars Best Practice
+  const doubleInteger = i => 2 * i;
