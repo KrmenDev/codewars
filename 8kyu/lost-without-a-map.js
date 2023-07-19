8fb1ba9cba460724069fafa56b37c575c421fa46
CarmenSM <contacto.cmsm@gmail.com>
2023-07-19 07:31:57 -0600

    challenge of the day

---
+++ 8kyu/lost-without-a-map.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+function maps(x){
+
+}
+
+//My solution
+function maps(x){
+    return x.map( n => n * 2)
+}
+
+//CodeWars
+maps = x => x.map(e => e * 2)
