f64062f07dd4ab25a8927b4c7ac1d2770c57590b
CarmenSM <contacto.cmsm@gmail.com>
2023-08-13 08:07:33 -0600

    challenge of the day

---
+++ 8kyu/welcome-to-the-city.js
@@ -0,0 +1,34 @@
+// DESCRIPTION:
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+function sayHello( name, city, state ) {
+}
+
+//P: 3 strings, always a string, never empty, the length will vary, name is an array
+//R: return concat string with one space between each
+//E: ['John', 'Smith'], 'Phoenix', 'Arizona' = Hello, John Smith! Welcome to Phoenix, Arizona!
+
+
+//my Solution
+function sayHello( name, city, state ) {
+    let fullName = name.join('')
+   return `Hello, ${fullName}! Welcome to ${city}, ${state}!`
+}
+
+
+
+//CodeWars
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
+
+
+function sayHello( name, city, state ) {
+    return 'Hello, ' + name.join(' ') + '! Welcome to ' + city + ', ' + state + '!';
+}
+
+
