a8321fdfc6d59d2405486e373ac62b73f57d4607
CarmenSM <contacto.cmsm@gmail.com>
2023-08-15 20:01:44 -0600

    challenge of the day

---
+++ 8kyu/rock-paper-scissors.js
@@ -0,0 +1,54 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+
+//My Solution
+const rps = (p1, p2) => {
+    if(p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock' ){
+        return "Player 1 won!"
+    }else if(p2 === 'rock' && p1 === 'scissors' || p2 === 'scissors' && p1 === 'paper' || p2 === 'paper' && p1 === 'rock' ){
+        return "Player 2 won!"
+    } else {
+        return "Draw!"
+    }
+};
+
+
+//CodeWars Best
+const rockPaperScissors = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+      return "Player 1 won!";
+    }
+    else {
+      return "Player 2 won!";
+    }
+  };
+
+//rules is an object with key: value
+//we can access it passing object['key']
+//we get the value if the vaule match the key return the p1 won
+//if they dont match return p2 won
+
+
+//codeWars
+const rockPaperScissorss = (p1, p2) => {
+    if (p1 == p2)
+      return 'Draw!';
+      
+     if (p1 == 'rock' && p2 == 'scissors') 
+       return 'Player 1 won!'
+     else if (p1 == 'scissors' && p2 == 'paper') 
+       return 'Player 1 won!'
+     else if (p1 == 'paper' && p2 == 'rock') 
+       return 'Player 1 won!'
+     else
+       return 'Player 2 won!';
+  };
