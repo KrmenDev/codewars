2a65c97459e4e9fc39e8967312d4f70e31477459
CarmenSM <contacto.cmsm@gmail.com>
2023-09-10 13:56:34 -0600

    challenge of the day

---
+++ 8kyu/find-the-first-non-consecutive-number.js
@@ -0,0 +1,42 @@
+// Description:
+
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+// If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
+
+// 1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
+
+//My Solution
+
+//P: Always numbers, always more than 1, never empty, unique numbers, ascending ordfer. positive and negative
+//R: A number that is not consecutive
+//E:
+//// [1,2,3,4,6,7,8] => 6
+//// [1,2,3,4] => null
+//P: check each num if they are all more than the previous by one return null if not return that num
+
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+  }
+
+
+  //CodeWars
+  function firstNonConsecutive (arr) {
+    let result = arr.find((val, index) => val !== index + arr[0]);
+
+    return (Number.isInteger(result)) ? result : null;
+}
+
