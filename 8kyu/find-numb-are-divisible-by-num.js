884ad0d564833f44530bf3e05c162ff75e896de6
CarmenSM <contacto.cmsm@gmail.com>
2023-09-11 22:49:56 -0600

    challenge of the day

---
+++ 8kyu/find-numb-are-divisible-by-num.js
@@ -0,0 +1,39 @@
+// Description:
+
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+// Example(Input1, Input2 --> Output)
+
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+//P: alway num, never empty, whole
+//R: attay of nums that are divisible by 2 argument
+//E:
+// [1,2,3,4,5,6],3 => [3,6]
+// [0,1,2,3,4,5,6], 4 => [0,4]
+// [0], 4) => [0]
+// [1,3,5], 2 => []
+//P:
+// check each arr elemnt if divisible by divisor
+//return new arr
+
+
+function divisibleBy(numbers, divisor){
+    return numbers.filter((num)=> num % divisor === 0)
+}
+
+
+
+//CodeWars
+function divisibleBy(numbers, divisor) {
+	var newArr = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] % divisor === 0) {
+			newArr.push(numbers[i]);
+		} 
+	}
+return newArr;
+}
+
+
+//CodeWars
+const divisibleBy = (numbers, divisor) => numbers.filter(number => !(number % divisor));
