2c2fa772adf80a07cc3f66985ce5b57f7999ae9b
CarmenSM <contacto.cmsm@gmail.com>
2024-06-19 16:35:47 -0600

    codewars challenge of the day

--- 8kyu/volume-of-cuboid.js
+++ 8kyu/volume-of-cuboid.js
@@ -4,13 +4,15 @@
 
 
 //My Solution
-//P: Always a num, always positive, always 3, never zero, never floated, never empty
+//P: Always a num, always positive, always 3 nums, never zero, never floated, never empty
 //R: single num, whole, positive, always > 0
 //E: 1,2,2 => 4
 //   6,2,5 => 60
 
 
 //P: the formula for calculating the volume of a cuboid: volume = l × w × h
+
+
 class Kata {
     static getVolumeOfCuboid(length, width, height) {
       return length * width * height
