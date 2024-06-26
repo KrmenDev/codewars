4758d3a6e0c23258989f0f4615d8df9e879e6b9e
CarmenSM <contacto.cmsm@gmail.com>
2024-06-26 11:48:58 -0600

    codewars challenge of the day

--- 8kyu/summation.js
+++ 8kyu/summation.js
@@ -8,7 +8,20 @@
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
 
-//My Solution
+
+//P: a whole number, never empty
+//R: the sum of 1 to num
+//E: 1 => 1
+//E: 2 (1 + 2) => 3 
+//E: 3 (1+2+3) => 6
+//P: Create an array with the consecutive numbers from 1 to num then sum all numbers and return result
+
+var summation = function (num) {
+  arr = Array.from({length:num}, (_,i) => i + 1)
+  return arr.reduce((acc, c) => acc + c, 0)
+}
+
+
 var summation = function (num) {
    let total = 0
    for (i = 0; i <= num; i++){
@@ -19,3 +32,5 @@ var summation = function (num) {
 
   //CodeWars Best Practice
   const summation = n => n * (n + 1) / 2;
+
+  
