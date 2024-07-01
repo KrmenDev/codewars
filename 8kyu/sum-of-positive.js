82f0382a4dfd4909ab92f9814cb62e0d025ecd1b
CarmenSM <contacto.cmsm@gmail.com>
2024-07-01 17:32:34 -0600

    codewars challenge of the day

--- 8kyu/sum-of-positive.js
+++ 8kyu/sum-of-positive.js
@@ -5,6 +5,22 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
+
+//P: array of numbers, can be positive or negative can be empty
+//R: the sum of all the positive numbers
+//E: [1,2,3,4,5] => 15
+//E: [] => 0
+//E: [-1, -2, -3] => 0
+//E: [-1, 2, 3, 4 -5] =>9
+//P: if the number is above 0 add to array the summ all
+
+
+function positiveSum(arr) {
+  let positives = arr.filter((e) => e > 0)
+  return positives.reduce((acc, c) => acc + c, 0)
+}
+
+
   //CodeWars Best Practice
 function positiveSum(arr) {
     let total = 0;
