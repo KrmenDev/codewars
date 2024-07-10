76d1c072609215c50647940a5e21b2b1b0914078
CarmenSM <contacto.cmsm@gmail.com>
2024-07-10 12:59:55 -0600

    codewars challenge of the day

--- 8kyu/simple-multiplication.js
+++ 8kyu/simple-multiplication.js
@@ -1,14 +1,18 @@
 // DESCRIPTION:
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
+
+//P: numbers, whole, positive
+//R: if even number return number times eigh if not times nine
+//E: 2 => 16
+//E: 1 => 9
+//E: 5 => 45
+//P: if number is even multiply by 8 if not multiply by 9
+
 function simpleMultiplication(number) {
-    // your code........
+    return number % 2 === 0 ? number * 8 : number * 9
 }
 
-//My Solution
-function simpleMultiplication(number) {
-    return  number % 2 === 0 ? number * 8 : number * 9
-}
 
 //CodeWars best practice
 function simpleMultiplication(n) {
