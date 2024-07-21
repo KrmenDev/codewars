9c607210ab57287fc6c1e934b0e2d5ed999027f8
CarmenSM <contacto.cmsm@gmail.com>
2024-07-20 18:03:24 -0600

    codewars challenge of the day

--- 8kyu/reversed-array-of-digits.js
+++ 8kyu/reversed-array-of-digits.js
@@ -2,16 +2,20 @@
 // Convert number to reversed array of digits
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
-// Example(Input => Output):
-// 35231 => [1,3,2,5,3]
-// 0 => [0]
 
-//My Solution
+//P: numbers, never empty always > 0
+//R: array of reversed numbers
+//E: 35231 => [1,3,2,5,3]
+//E: 0 => [0]
+//P: pass numbers to array then reverse the order and return
+
+
 function digitize(n) {
-    //code here
-  }
+  let str = String(n)
+  return [...str].map((e) => Number(e)).reverse()
+}
 
-  //My Solution
+
   function digitize(n) {
     let numArray = n.toString().split('').reverse()
     return numArray.map(e => parseInt(e))
