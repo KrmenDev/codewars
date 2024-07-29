23804c61bbbe9a467484e304abf32f8960441307
CarmenSM <contacto.cmsm@gmail.com>
2024-07-29 13:13:12 -0600

    codewars challenge of the day

--- 8kyu/remove-string-spaces.js
+++ 8kyu/remove-string-spaces.js
@@ -8,13 +8,21 @@
 // "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
 // "8aaaaa dddd r     " -> "8aaaaaddddr"
 
+//P:string with lower and uppercase, never empty 
+//R: string with no spaces
+//E: 'aa b c' => 'aabc'
+//E: 'C a r m e n' => 'Carmen'
+//P: use method remove all and return string
 
-//My Solution
 function noSpace(x){
-    return x.replaceAll(' ', '')
-  }
+  return x.replaceAll(' ', '')
+}
+
 
   //CodeWars Best practice
   function noSpace(x){
     return x.replace(/\s/g, '');
   }
+
+  //codeWars
+  function noSpace(x){return x.split(' ').join('')}
