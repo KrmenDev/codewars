499d1e88ed5e69fc7560f550b39cc6cf0e43d5a0
CarmenSM <contacto.cmsm@gmail.com>
2024-07-30 11:27:22 -0600

    codewars challenge of the day

--- 8kyu/reduce-but-grow.js
+++ 8kyu/reduce-but-grow.js
@@ -2,15 +2,17 @@
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//P: integers, positive never empty 
+//R: the result of multiplying all integers
+//E: [1,2,3] => 6
+//E: [4,1,1,1,4] => 16
+//E: [2, 2, 2, 2, 2, 2] => 64
+//P: maybe use reduce to end up with the result
+
 function grow(x){
-
+  return x.reduce((acc, c) => acc * c, 1)
 }
-
-//My Solution
-function grow(x){
-    return x.reduce((a, b) => a * b)
-}
-
 
 
 //CodeWars Best Practice
