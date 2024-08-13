7eb6dc4dd910956d6bcb39a476abf44fe2c06a44
CarmenSM <contacto.cmsm@gmail.com>
2024-08-13 13:20:33 -0600

    codewars challenge of the day

--- 8kyu/is-there-a-vowel.js
+++ 8kyu/is-there-a-vowel.js
@@ -10,19 +10,38 @@
 //E: [118,117,120,121] = [118, "u", 120, 121]
 //  [113, 113, 103, 121, 121, 101,107,103] => [113, 113, 103, 121, 121, "e",107,103]
 //P: check each element in array, if matches the value of vowel code replace and return array
-    
-  function isVow(a){
-    return a.map((e) => e === 97 ? "a" 
-    : e === 101 ? "e" 
-    : e == 105 ? "i"
-    : e === 111 ? "o"
-    : e === 117 ? "u"
-    : e )
-  };
 
+function isVow(a){
+  return a.map((e) => 
+    e === 97 ? 'a' 
+  : e === 101 ? 'e' 
+  : e === 105 ? 'i' 
+  : e === 111 ? 'o' 
+  : e === 117 ? 'u' 
+  : e)
+}
 
 
 //CODEWARS BEST
 const v = {"97":"a","101":"e","105":"i","111":"o","117":"u"}
 const isVow = a => a.map( x => v[x] || x)
+
+
+
+//codewars
+function isVow(a){
+  for (var i=0, l=a.length; i<l; ++i)
+  {
+    var char = String.fromCharCode(a[i])
+    if ('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+  }
+  
+  return a;
+}
+
+
+//codewars
+const isVow = a => 
+  a.map(x => 'aeiou'.includes(y = String.fromCharCode(x)) ? y : x)
 
