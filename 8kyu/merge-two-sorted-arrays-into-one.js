eeaf1ec14da1f6d200e01aeabe99982a7246af54
CarmenSM <contacto.cmsm@gmail.com>
2024-08-12 19:19:15 -0600

    codewars challenge of the day

--- 8kyu/merge-two-sorted-arrays-into-one.js
+++ 8kyu/merge-two-sorted-arrays-into-one.js
@@ -18,7 +18,13 @@
 //[1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //[1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
-//P: Sort arrays merge if num is not duplicate
+//P: concat the two arrays eliminate duplicated nums and sort them 
+
+function mergeArrays(arr1, arr2) {
+  let mergedArrays = arr1.concat(arr2)
+  return [...new Set(mergedArrays)].sort((a,b) => a - b)
+}
+
 
 function mergeArrays(arr1, arr2) {
   let arr3 = arr1.concat(arr2).sort((a,b) => a-b)
