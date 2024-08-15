48ab7ece14e89f0c2f609345dc9b82397307d6ff
CarmenSM <contacto.cmsm@gmail.com>
2024-08-15 11:18:06 -0600

    codewars challenge of the day

--- 8kyu/is-he-gonna-survive.js
+++ 8kyu/is-he-gonna-survive.js
@@ -1,17 +1,18 @@
 // DESCRIPTION:
 // A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
 
-// Return true if yes, false otherwise :)
+
+//P: number, always positive, never empty
+//R: Return true if yes, false otherwise :)
+//E: 10, 5 => true
+//E: 7, 4 => false
+//E: 100, 40 => true
+//P: if the number of bullets is less than the dragons multiplied by two return boolean
 
 function hero(bullets, dragons){
-    //Get Coding!
+    return bullets >= (dragons * 2) ? true : false
     }
 
-
-//My Solution
-function hero(bullets, dragons){
-    return bullets >= dragons * 2 ? true : false
-}
 
 
 //CodeWars Best Practice
@@ -22,6 +23,7 @@ function hero(bullets, dragons){
 
 //CodeWars
   function hero(bullets, dragons){
-    //Get Coding!
     return (bullets / 2 >= dragons) ? true : false;
     }
+
+    
