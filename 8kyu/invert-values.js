f799a715e75c213ce6c7efa47cd2b3e3acf64d2b
CarmenSM <contacto.cmsm@gmail.com>
2024-08-16 07:44:19 -0600

    codewars challenge of the day

--- 8kyu/invert-values.js
+++ 8kyu/invert-values.js
@@ -5,12 +5,17 @@
 // invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 // invert([]) == []
 // You can assume that all values are integers. Do not mutate the input array/list.
+
+//P: array of numbers, positives and negatives, never empty
+//R: inverted numbers if positive return negative
+//E: [1,-2,3] => [-1, 2, -3]
+//E: [-5, -6, -8, -9, 2, 8, 1] => [5, 6, 8, 9, -2, -8, -1]
+//P: check each number and return its opposite, use map
 function invert(array) {
-    return ;
+    return array.map((e) => Math.abs(e) === e ? -Math.abs(e) : Math.abs(e))
  }
 
 
- //My solution
  function invert(array) {
     return array.map( e => e === +e ? -e : +e )
  }
