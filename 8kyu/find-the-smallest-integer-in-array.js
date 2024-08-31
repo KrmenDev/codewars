fa735be7581d1639376fab0034c233dc06b39de6
CarmenSM <contacto.cmsm@gmail.com>
2024-08-30 18:16:57 -0600

    codewars challenge of the day

--- 8kyu/find-the-smallest-integer-in-array.js
+++ 8kyu/find-the-smallest-integer-in-array.js
@@ -7,6 +7,19 @@
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
+
+//P: array of numbers, never empty, can be negative
+//R: the smallest number
+//E: [1,56,232,12,8] => 1
+//E: [78,56,232,12,0] => 0
+//P: sort array and return smallest
+
+
+function findSmallestInt(arr) {
+  return Math.min(...arr);
+}
+
+
 //My solution
 class SmallestIntegerFinder {
     findSmallestInt(args) {
@@ -20,5 +33,14 @@ class SmallestIntegerFinder {
   class SmallestIntegerFinder {
     findSmallestInt(args) {
       return Math.min(...args)
+    }
+  }
+
+  //CodeWars
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return args.reduce(function(prev, curr, index, array) {
+        return prev < curr ? prev : curr;
+      });
     }
   }
