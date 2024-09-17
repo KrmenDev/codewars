a27e7e0436db3d531a455bdf71cce93c08fcc36f
CarmenSM <contacto.cmsm@gmail.com>
2024-09-17 13:40:16 -0600

    codewars challenge of the day

---
+++ 8kyu/say-hello.js
@@ -0,0 +1,33 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+//P: string, never empty
+//R: Hello + name
+//E: 'Mr. Spock' => 'Hello, Mr. Spock'
+//E: 'Dr. McCoy' => 'Hello, Dr. McCoy'
+//E: 'Liutenant Uhura' => 'Hello, Liutenant Uhura'
+//P: Add 'Hello + string'
+
+function sayHello(name) {
+    return 'Hello, ' + name
+  }
+
+
+
+//Codewars
+const sayHello = name => `Hello, ${name}`;
+
+
+//Codewars
+function sayHello (name) {
+    return 'Hello, ' +  name;
+  }
+
+
+//Codewars
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
