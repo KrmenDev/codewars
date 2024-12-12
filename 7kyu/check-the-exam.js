8f4dfc571c02dc7e27baf6a69f1da4a54512fb6f
CarmenSM <contacto.cmsm@gmail.com>
2024-12-12 17:26:20 -0600

    Codewars challenge of the day

--- 7kyu/check-the-exam.js
+++ 7kyu/check-the-exam.js
@@ -19,21 +19,59 @@
 //if the same num +4 if not -1 if empty or 0 return 0
 
 
-
-
-
-
-function checkExam(array1, array2) {
-    let result = 0
-    array1.every((e,i) => e === array2[i] ? result += +4 : result += -1)
-    return result
-   }
-
-
    function checkExam(array1, array2) {
-    let result = 0
-    array1.every((e,i) => e === array2[i] ?  : result += -1)
-    return result
+        let result = []
+        array1.forEach((e,i) => array2[i] == "" ? 0 : e === array2[i] ? result.push(4) : result.push(-1))
+        if(result.reduce((acc,c) => acc + c, 0) < 0){
+            return 0
+        }else{
+            return result.reduce((acc,c) => acc + c), 0
+        }
    }
 
-   .reduce((acc,c) => acc + c, 0)
+
+
+//Codewars
+function checkExam(array1, array2) {
+    var score = 0; 
+      for (var i = 0; i < array1.length; i++){  
+        if (array1[i] == array2[i]) {    
+        score += 4;     
+        }
+          else if (array2[i] === ""){
+              score += 0
+          }    
+        else {        
+        score -= 1       
+        }      
+      }     
+      if (score < 0) {      
+        score = 0        
+      }       
+     return score
+     
+    }
+
+
+
+//Codewars
+function checkExam(array1, array2) {
+    const reducer = (a, e, idx) => {
+      if (e === "") {
+        return a;  
+      }
+      
+      if (e === array1[idx]) {
+        return a += 4;
+      }
+      
+      return --a;
+    }
+    const score = array2.reduce(reducer, 0);
+    return score < 0 ? 0 : score;
+  }
+
+
+  
+//Codewars
+checkExam = (x, y) => (x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0;
