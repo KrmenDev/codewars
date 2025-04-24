c5ed0e9101b227f9ccabc936dc231a74e0f3df33
CarmenSM <contacto.cmsm@gmail.com>
2025-04-24 16:47:20 -0600

    Codewars challenge of the day

---
+++ 8kyu/switch-it-up.js
@@ -0,0 +1,67 @@
+// Description:
+
+// When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+
+// Output: "One".
+
+// If your language supports it, try using a switch statement.
+
+
+
+function switchItUp(number){
+    switch(number){
+        case 0: return "Zero";
+        case 1: return "One";
+        case 2: return "Two";
+        case 3: return "Three";
+        case 4: return "Four";
+        case 5: return "Five";
+        case 6: return "Six";
+        case 7: return "Seven";
+        case 8: return "Eight"
+        case 9: return "Nine";
+        case 10: return "Ten";
+    }
+}
+
+
+//Codewars
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+
+
+
+//Codewars
+function switchItUp(number){
+    switch(number) {
+      case 0:
+        return 'Zero';
+      case 1:
+        return 'One';
+      case 2:
+        return 'Two';
+      case 3:
+        return 'Three';
+      case 4:
+        return  'Four';
+      case 5:
+        return 'Five';
+      case 6:
+        return 'Six';
+      case 7:
+        return 'Seven';
+      case 8:
+        return 'Eight';
+      case 9:
+        return 'Nine';
+      default:
+        return 'Unknown number';
+    }
+  }
+
+
+//Codewars
+function switchItUp(n){
+    return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+  }
