50a65e2b36bdbd8d3fd2450f734d7ee48dc7fed3
CarmenSM <contacto.cmsm@gmail.com>
2025-08-21 14:23:01 -0600

    Codewars challenge of the day

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,34 @@
+// Description:
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+//return 5 without using numbers or signs
+
+
+function unusualFive() {
+    let arr = ['a','b', 'c','d','e','f' ]
+  return arr.indexOf('f')
+}
+
+
+//Codewars
+function unusualFive() {
+  return 'fucku'.length
+}
+
+
+
+//Codewars
+function unusualFive() {
+  return "jesus".length;
+}
+
+
+
+//Codewars
+const unusualFive = () =>
+  Math.hypot(Math.ceil(Math.PI), Math.floor(Math.PI));
