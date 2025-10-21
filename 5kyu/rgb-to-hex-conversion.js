b0c39cddfd4f6e537f5884ff8b452a8519a6a104
CarmenSM <contacto.cmsm@gmail.com>
2025-10-20 21:01:31 -0600

    Codewars challenge of the day

---
+++ 5kyu/rgb-to-hex-conversion.js
@@ -0,0 +1,49 @@
+// Description:
+
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+// Examples (input --> output):
+
+// 255, 255, 255 --> "FFFFFF"
+// 255, 255, 300 --> "FFFFFF"
+// 0, 0, 0       --> "000000"
+// 148, 0, 211   --> "9400D3"
+
+
+
+function rgb(r, g, b) {
+    return "FFFFFF";
+}
+
+
+//Codewars
+function rgb(r, g, b){
+	return toHex(r)+toHex(g)+toHex(b);
+}
+
+function toHex(d) {
+    if(d < 0 ) {return "00";}
+    if(d > 255 ) {return "FF";}
+    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
+
+
+
+
+//Codewars
+function rgb(r, g, b){
+  return [r,g,b].map(function(x) {
+    return ('0'+Math.max(0, Math.min(255, x)).toString(16)).slice(-2);
+  }).join('').toUpperCase();
+}
+
+
+
+//Codewars
+const rgb = (...arg) => arg
+  .map(it => Math.max(Math.min(it, 255), 0)
+    .toString(16)
+    .padStart(2, '0')
+    .toUpperCase()
+   ).join('');
